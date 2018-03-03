137cb61e8f7c841b3e73b707f85740ed076d4b3c
mirelatv <noelia.mirela@gmail.com>
2018-03-03 00:05:16 -0500

    subiendo public , con index.html,subiendo estructura de proyecto

---
+++ public/js/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+  //jaalando  data
+
+
+  //hallando categorias  que  se  colocaran en listas
+  $.ajax({
+    url: 'https://api.mercadolibre.com/categories/MPE1574',
+    success: function (data) {
+      var categorias = data.children_categories
+      console.log(categorias)
+      categorias.forEach(function (element) {
+        const li = `<li class="lisCategorias" data-id=${element.id}><a href="">${element.name}</a></li>`
+        $(".fatherList").append(li);
+      });
+    }
+
+  });
+  // jalando subcategorias
+  $.ajax({
+    url: 'https://api.mercadolibre.com/sites/MPE/search?category=MPE1574',
+    success: function (data) {
+      var children = data.results
+      console.log(children);
+
+      children.forEach(function (element) {
+        const div = ` <div id= ${element.category_id} class="listProductos">
+       <img src="${element.thumbnail} alt="">
+       <p>${element.title}</p>
+       <p>${element.price}</p>
+       <p>${element.category_id}</p>
+       </div>`
+        $(".row").append(div);
+       // $(".listProductos").hide();
+
+      })
+
+    }
+  });
+
+  //evento que relaciona  la categoria  con las subcategorias.
+  
+  //$(".lisCategorias").click(function() {
+   // if($(".lisCategorias").attr( "data-id" )=== $(".listProductos").attr("id")){
+   //   $( ".lisProductos" ).show()
+   // }
+  //});
+
+
+})
+
+
+// ITERANDO CATEGORIAS
+
+//
+
+
