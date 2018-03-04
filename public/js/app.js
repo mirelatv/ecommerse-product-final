77adf619ef1ef5c5d05f87049811c0c231d5e999
Danielalab <olabolacrayola@hotmail.com>
2018-03-03 23:48:59 -0500

    añade boton de Paypal

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
 
-  //jaalando  data
-
+  // configurando sidebar materialize
+  $(".button-collapse").sideNav();
+  $('.button-collapse#shopping-cart').sideNav({
+    edge: 'right', // Choose the horizontal origin
+  });
 
   //hallando categorias  que  se  colocaran en listas
   $.ajax({
@@ -10,12 +13,14 @@ $(document).ready(function () {
       var categorias = data.children_categories
       console.log(categorias)
       categorias.forEach(function (element) {
-        const li = `<li  class="lisCategorias" data-id=${element.id}><a href="">${element.name}</a></li>`
+        const li = `<li  class="lisCategorias" data-id=${element.id}><a href="/${element.id}">${element.name}</a></li>`
         $(".right").append(li);
+        $("#mobile-demo").append(li);
       });
     }
 
   });
+
   // jalando subcategorias
   $.ajax({
     url: 'https://api.mercadolibre.com/sites/MPE/search?category=MPE1574',
@@ -31,7 +36,7 @@ $(document).ready(function () {
        <p class="center-align">${element.category_id}</p>
        </div>`
         $(".row").append(div);
-       // $(".listProductos").hide();
+        // $(".listProductos").hide();
 
       })
 
@@ -39,19 +44,53 @@ $(document).ready(function () {
   });
 
   //evento que relaciona  la categoria  con las subcategorias.
-  
+
   //$(".lisCategorias").click(function() {
-   // if($(".lisCategorias").attr( "data-id" )=== $(".listProductos").attr("id")){
-   //   $( ".lisProductos" ).show()
-   // }
+  // if($(".lisCategorias").attr( "data-id" )=== $(".listProductos").attr("id")){
+  //   $( ".lisProductos" ).show()
+  // }
   //});
 
+  // Inicia Configuración de API PayPal
+  paypal.Button.render({
+    env: 'production', // Or 'sandbox',
+    client: {
+      sandbox: 'AUbwyqI0Qv7-Jx560ktdg_rwLNaT35-a3FYA7-SdVf9i7dTbYD56hMqXC4YGQZr0RHMHOKhHlUJblkfk',
+      production: 'AWStTtI9jh03288Nu2oVlmmgFz-3lXcnm7Jtwj4QZ55oPw2m8cDK4vsyU_vORxwtEfjoygFEOAiM0LWO'
+    },
+    locale: 'es_ES',
+    commit: true, // Show a 'Pay Now' button
+    style: {
+      color: 'blue',
+      size: 'responsive',
+      shape: 'rect',
+    },
 
+    payment: function (data, actions) {
+      return actions.payment.create({
+        payment: {
+          transactions: [{
+            amount: {
+              total: '1.00', // enviar precio en esta función 
+              currency: 'USD'
+            }
+          }]
+        }
+      });
+    },
+
+    onAuthorize: function (data, actions) {
+      return actions.payment.execute().then(function (payment) {
+        // The payment is complete!
+        // You can now show a confirmation message to the customer
+      });
+    },
+  }, '#paypal-button');
+
+  // Termina Configuración de API PayPal  
 })
 
 
 // ITERANDO CATEGORIAS
 
 //
-
-
